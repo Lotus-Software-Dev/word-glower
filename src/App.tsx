d1638b1b36f7560947aa1eb060ebca0777dc2392
SaMuCoder <samucybert@gmail.com>
2023-07-01 21:03:45 +0000

    Added: SorceText, TextArea and Generate Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(count)
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="text-5xl">Word Glower</div>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
-      <div>{count}</div>
+    <div className="flex flex-col justify-center items-center h-screen gap-20">
+      <div className="text-5xl font-sans text-[#6527BE]">Word Glower</div>
+      <div className="flex flex-col justify-center w-8/12 gap-3">
+        <label
+          htmlFor="sourceText"
+          className="text-xl font-sans flex justify-start text-[#6527BE]"
+        >
+          Source Text
+        </label>
+        <textarea
+          id="sourceText"
+          className="h-[500px] border border-gray-300 rounded-lg text-start"
+        />
+        <div className="flex justify-end">
+          <button className="bg-[#9681EB] hover:bg-sky-700 rounded-lg text-white py-3 px-4">
+            Generate
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
